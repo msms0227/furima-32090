7dbfa8a05cfe9c713a25df756377995937c33f90
msms0227 <musajudo@icloud.com>
2020-12-05 20:31:43 +0900

    Rubocop実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,24 +4,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
-         validates :birthday, presence: true
-         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '半角英数字混合で入力してください' } 
+  validates :nickname, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '半角英数字混合で入力してください' }
 
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-          validates :last_name, presence: true
-          validates :first_name, presence: true
-         end
-         
-         with_options presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,message: "全角カタカナのみで入力して下さい" } do
-          validates :last_name_kana, presence: true
-          validates :first_name_kana, presence: true
-        end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name, presence: true
+    validates :first_name, presence: true
+  end
 
-         has_many :items
-         has_many :buy_records
+  with_options presence: true, format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナのみで入力して下さい' } do
+    validates :last_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
 
+  has_many :items
+  has_many :buy_records
 end
-
