74461e4604ed5bcf244f6ac3f24a418c14b5aa31
msms0227 <musajudo@icloud.com>
2020-12-06 17:51:06 +0900

    itemテーブル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  #has_one    :buy_record
+  has_one_attached :image
+
+  validates :name,presence: true
+  validates :data,presence: true
+  validates :category_id,presence: true
+  validates :status_id,presence: true
+  validates :delivery_fee_id,presence: true
+  validates :area_id,presence: true
+  validates :days_id,presence: true
+  validates :image,presence: true
+
 end
--- db/migrate/20201205120954_create_items.rb
+++ db/migrate/20201205120954_create_items.rb
@@ -3,6 +3,14 @@ class CreateItems < ActiveRecord::Migration[6.0]
     create_table :items do |t|
 
       t.timestamps
+      t.string :name, null: false
+      t.text :data, null: false
+      t.integer :category_id, null: false
+      t.integer :status_id,null: false
+      t.integer :delivery_fee_id,null: false
+      t.integer :area_id,null: false
+      t.integer :days_id,null:false
+      t.references :user,foreign_key: true
     end
   end
 end
