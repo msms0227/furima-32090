07cbaa6f1589fae8d5e5d63b44f8ee4f4e25ddc7
msms0227 <musajudo@icloud.com>
2020-12-07 12:17:51 +0900

    商品投稿画面遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
+
   def index
   end
+
+  def new
+    @item=Item.new
+  end
+
+  def create
+    if Item.create(item_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
+private
+
+  def item_params
+    params.require(:item).permit(:name, :data, :category_id, :image, :status_id, :delivery_fee_id, :area_id, :days_id)
+  end
