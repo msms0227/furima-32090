edd1943c3cce4e5fdac689ae181572ec54a36279
msms0227 <musajudo@icloud.com>
2020-12-09 11:50:15 +0900

    テストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,10 +29,22 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
 
+      it 'category_idが1ではいけない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+
       it 'status_idが空ではいけない' do
         @item.status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+
+      it 'status_idが1ではいけない' do
+        @item.status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
       end
 
       it 'delivery_fee_idが空ではいけない' do
@@ -47,16 +59,40 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Area can't be blank")
       end
 
+      it 'area_idが1ではいけない' do
+        @item.area_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area must be other than 1")
+      end
+
       it 'days_idが空ではいけない' do
         @item.days_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Days can't be blank")
       end
 
+      it 'days_idが1ではいけない' do
+        @item.days_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days must be other than 1")
+      end
+
       it 'priceが空ではいけない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'priceが300以上でなければいけない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price 300円以上で入力して下さい")
+      end
+
+      it 'priceが9999999以下でなければいけない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price 9999999以下で入力して下さい")
       end
     end
   end
