058dc80b3e7aabec1e037bea8c5aa9b74bb768e8
msms0227 <musajudo@icloud.com>
2020-12-11 19:07:15 +0900

    url入力時ルーティング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit]
+  before_action :move_to_index,only:[:edit]
   def index
     @item = Item.all.order('created_at DESC')
   end
@@ -24,7 +25,7 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      render :show
+      redirect_to item_path(@item)   
     else
       render :edit
     end
@@ -39,5 +40,12 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :data, :category_id, :image, :status_id, :delivery_fee_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item=Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id 
+      redirect_to action: :index
+    end
   end
 end
