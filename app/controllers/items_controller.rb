84a734968b0f007cf4c4efa7e5d4c26b894c554b
msms0227 <musajudo@icloud.com>
2020-12-15 14:51:36 +0900

    エラーハンドリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,8 +14,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
   end
 
   def create
